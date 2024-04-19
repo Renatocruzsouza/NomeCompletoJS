a66ff48fc49f608b3ee5baa84f785e164046b96d
renato souza <e107ti@etec.sp.gov.br>
2024-04-18 21:20:38 -0300

    NomeCompletoJS

---
+++ script.js
@@ -0,0 +1,5 @@
+let nome = prompt("digite seu nome");
+let sobrenome = prompt("digite seu sobrenome");
+
+alert("seu nome completo" + nome + " " + sobrenome);
+alert(sobrenome.toLocaleUpperCase() + ", " + nome);
